0bec6573a1d89c3d465d8a315b675f7335866564
Lamia Haider <lamia.haider@gmail.com>
2022-01-25 10:12:55 -0500

    Controller able to add transactions to storage

--- db/controller.js
+++ db/controller.js
@@ -1,14 +1,18 @@
-// var userPoints = require("../server.js");
 var userPoints = [];
 
-//add transaction route
+//add transaction route, convert date from string to date object
 const addTransaction = (request, response) => {
   let obj = request.body;
+  var date = new Date(obj.timestamp);
+  obj.timestamp = date;
   userPoints.push(obj);
   response.json(userPoints);
 };
 
 //spending points route
+//  We want the oldest points to be spent first
+//(oldest based on transaction timestamp not the order they’re received)
+//  We want no payer's points to go negative.
 const spendingPoints = (request, response) => {};
 //points balance route
 const fetchAll = (request, response) => {};
